87a64555e8f4247906a876ce7d2560089aeb1fb8
AndreKuru <andrewilliam13@gmail.com>
2017-09-26 08:16:13 -0300

    Compiling

---
+++ EventOccurrence.cpp
@@ -0,0 +1,1 @@
+#include "EventOccurrence.h"
---
+++ EventOccurrence.h
@@ -0,0 +1,25 @@
+#ifndef TRAFFICSIMULATOR_EVENTOCCURRENCE_H
+#define TRAFFICSIMULATOR_EVENTOCCURRENCE_H
+
+#include <ctime>
+
+enum Events {
+    ARRIVAL_OF_NEW_CAR,
+    SIGNAL_LIGHT_SHIFT,
+    ARRIVAL_OF_CAR_IN_SEMAPHORE,
+    TRACK_EXCHANGE};
+
+class EventOccurrence {
+ public:
+    explicit EventOccurrence(Events event, clock_t time) :
+            event_{event},
+            time_{time} {}
+
+ private:
+    Events event_;
+    std::clock_t time_;
+
+};
+
+
+#endif //TRAFFICSIMULATOR_EVENTOCCURRENCE_H
